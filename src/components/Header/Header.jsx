3213a0c142f5cc962cf0863559f518860d8cb0c1
parth650 <130958174+parth650@users.noreply.github.com>
2023-07-13 15:34:12 -0700

    Added the header section and created components

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <section className='h-wrapper'>
+        <div className='flexCenter paddings innerWidth h-container'>
+            <img src='./logo.png' alt='logo' width={100}/>
+
+            <div className=' flexCenter h-menu'>
+                <a
+                    href=''
+                >
+                    Residences
+                </a>
+                <a href=''>Our Values</a>
+                <a href=''>Contact Us</a>
+                <a href=''>Get Started</a>
+                <button className='button'>
+                    <a href=''>Conatct Us</a>
+                </button>
+            </div>
+        </div>
+
+    </section>
+  );
+}
+export default Header
