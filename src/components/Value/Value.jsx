540dc42ee4eaad96c769e54003aa3c70d4b98963
parth650 <130958174+parth650@users.noreply.github.com>
2023-08-07 16:11:11 -0700

    Added CSS and grid for details under value.jsx

--- src/components/Value/Value.jsx
+++ src/components/Value/Value.jsx
@@ -10,6 +10,7 @@ import {
 import "react-accessible-accordion/dist/fancy-example.css"
 import {MdOutlineArrowDropDown} from 'react-icons/md'
 import './Value.css'
+import data from "../../utils/accordion"
 
 const Value = () => {
   return (
@@ -25,12 +26,44 @@ const Value = () => {
             {/*right-side*/}
             <div className="flexColStart v-right">
                 <span className='orangeText'>Our Value</span>
-                <span>Value We Give Our Clients</span>
-                <span>
+                <span className='primaryText'>Value We Give Our Clients</span>
+                <span className='secondaryText'>
                     We are always ready to help by providing the best service for you.
                     <br/>
                     We believe a good place to live can make your life better
                 </span>
+
+                <Accordion 
+                className='accordion'
+                allowMultipleExpanded={false}
+                preExpanded={[0]}
+                >
+                    {
+                        data.map((item, i)=> {
+                            return (
+                                <AccordionItem className="accordionItem" key={i} uuid={i}>
+                                    <AccordionItemHeading>
+                                        <AccordionItemButton>
+                                            <div className="flexCenter icon">{item.icon}</div>
+                                            <span className="primaryText">
+                                                {item.heading}
+                                            </span>
+                                            <div className="flexCenter icon">
+                                                <MdOutlineArrowDropDown size={20}/>
+                                            </div>
+                                        </AccordionItemButton>
+                                    </AccordionItemHeading>
+                                    <AccordionItemPanel>
+                                        <p className="secondaryText">{item.detail}</p>
+                                    </AccordionItemPanel>
+                                </AccordionItem>
+
+                            )
+                        })
+                    }
+
+
+                </Accordion>
             </div>
         </div>
     </section>
